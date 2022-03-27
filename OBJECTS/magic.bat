76b4a5168863de6f1bc3993f8840a898b5e9eb75
Andrew Dansby <andydansby@gmail.com>
2022-03-27 18:24:13 -0400

    Add files via upload

---
+++ OBJECTS/magic.bat
@@ -0,0 +1,21 @@
+rem combine all object files
+
+if not exist "RAM0.o" (
+call error.bat
+)
+if not exist "uncontended.o" (
+call error.bat
+)
+
+
+z80asm --output=ramALL.o RAM0.o uncontended.o
+
+if not exist "ramALL.o" (
+call error.bat
+)
+
+z80nm ramALL.o > ramALL.txt
+	@rem output.txt will have the listing of routines
+
+REM move the object file created above into the RAMMAIN directory to compile the test.c file
+copy "ramALL.o" "..\UNCONTENDED\"
